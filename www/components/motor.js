b3b5f63e48b13b1f0e5be0cbe23195b26e4664b9
reduardadsilv <eduardadasilvarodrigues766@gmail.com>
2023-03-17 04:56:01 +0900

    Project's Initial Commit

---
+++ www/components/motor.js
@@ -0,0 +1,12 @@
+function calcularcosumo() {
+    var resultado, km, litros
+
+    km = document.getElementById('km').value 
+    litros = document.getElementById('litros').value
+
+    resultado = km / litros
+
+    document.getElementById('resultado').textContent = resultado + "km/l" 
+
+}
+
